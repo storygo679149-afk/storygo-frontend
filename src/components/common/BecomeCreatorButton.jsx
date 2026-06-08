7cb43602bcad87226b4de311ebfca35541ce608b
storygo679149-afk <storygo679149@gmail.com>
2026-06-08 17:11:59 +0530

    Update BecomeCreatorButton.jsx

--- src/components/common/BecomeCreatorButton.jsx
+++ src/components/common/BecomeCreatorButton.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { useAuthContext } from '../../context/AuthContext';
 import { useNavigate } from 'react-router-dom';
+import './BecomeCreatorButton.css'; // optional
 
 const BecomeCreatorButton = () => {
   const { user, becomeCreator } = useAuthContext();
